c8d9319eb1e0923f6b8342843b79d6e3dc76f3bc
diomids2 <73174868+diomids2@users.noreply.github.com>
2021-07-21 11:13:33 -0300

    versao1.0
    futuro site pra karem

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+        CRIANDO HISTORIAS JUNTO COM VOCÊ.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          teste
+        </a>
+        <div className="App-Menu"></div>
+      </header>
+      <main className="App-Main">
+     
+      </main>
+    </div>
+  );
+}
+
+export default App;
